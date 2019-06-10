bf67ec74e742998401f6677ac976dc4f35da06ad
artinsepanta <faribasalehi@hotmail.com>
2019-06-10 18:20:37 -0500

    saving progress

--- src/App.js
+++ src/App.js
@@ -2,25 +2,68 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  function userCard(props){
+
+    let info =(
+      <div>
+        <span>{props.user.name.first}</span> 
+        {''}
+        <span>{props.user.name.last}</span>
+      </div>
+    )
+
+    return (
+      <div style={{marginBottom:'40px'}}>
+        <img   src={props.user.picture.large}></img>
+        <div>
+          {props.hide===false ? info : null}
+        </div>  
+        <button onClick={props.onClick}>Hide Details</button>
+      </div>
+    );
+  }
+
+  class App extends Component{
+    constructor(props) {
+      super(props);
+      this.state = {
+        result:[],
+        isHidden:true
+      };
+    }
+    componentDidMount() {
+      fetch('https://randomuser.me/api?results=25')
+        .then((res)=>{
+            return res.json()
+        })
+        .then((json)=> {
+          this.setState({
+            results:json.results
+          })
+        })
+    }
+
+    onClick=(event) => {
+      this.setState({
+        isHidden:false
+      });
+    }
+
+    render() {
+      return (
+        <div className="App">
+          {this.state.results.map((users, index) =>
+          <userCard
+            key ={index} 
+            users={user}
+            onClick={this.onClick}
+            hide={this.state.isHidden}
+          />
+      )}
+        </div>
+    );
+  }
 }
 
 export default App;
